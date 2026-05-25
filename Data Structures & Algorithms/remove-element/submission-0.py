fe8f30398881f8e0ba2d742c7e5ae26804e7654e
Abu Ubaida Akash <abu.ubaida1206@gmail.com>
2026-05-25 18:06:21 -0400

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            if nums[i]==val:
+                popped = nums.pop(i)
+                nums.append(popped)
+                count+=1
+        return len(nums)-count
